089a5b0e9c31a746b29a8a735a43b4cafc727f5a
awilliamson10 <aw.williamson10@gmail.com>
2023-11-02 08:31:29 -0400

    mixed precision

--- train.py
+++ train.py
@@ -25,6 +25,7 @@ def main(args):
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         log_with="wandb" if args.wandb else None,
+        mixed_precision=args.precision,
     )
 
     if accelerator.is_main_process:
@@ -35,43 +36,30 @@ def main(args):
             accelerator.print(f"Output directory: {args.output_dir}")
             os.makedirs(args.output_dir, exist_ok=True)
 
+        if args.wandb:
+            accelerator.init_trackers(
+                project_name="clipora",
+                config={
+                    "learning_rate": args.learning_rate,
+                    "dataset": args.instance_data_dir,
+                    "epochs": args.epochs,
+                },
+            )
+
     # Load the model and tokenizer
     model, preprocess_train, _ = open_clip.create_model_and_transforms(
         f"hf-hub:{args.model_name}",
+        precision=args.precision,
+        device=accelerator.device,
     )
 
-    # Get the TextEncoder and ImageEncoder
-    text_encoder = model.transformer
-    text_encoder.requires_grad_(False)
-
-    image_encoder = model.visual
-    image_encoder.requires_grad_(False)
-
-    text_encoder_lora_params, _ = inject_trainable_lora(
-        text_encoder,
+    # Inject LoRA
+    model.requires_grad_(True)
+    model_lora_params, _ = inject_trainable_lora(
+        model,
         target_replace_module=["MultiheadAttention"],
         r=args.lora_rank,
     )
-
-    for _up, _down in extract_lora_ups_down(
-        text_encoder, target_replace_module=["MultiheadAttention"]
-    ):
-        print("Before training: text encoder First Layer lora up", _up.weight.data)
-        print("Before training: text encoder First Layer lora down", _down.weight.data)
-        break
-
-    image_encoder_lora_params, names = inject_trainable_lora(
-        image_encoder.transformer,
-        target_replace_module=["MultiheadAttention"],
-        r=args.lora_rank,
-    )
-
-    for _up, _down in extract_lora_ups_down(
-        image_encoder.transformer, target_replace_module=["MultiheadAttention"]
-    ):
-        print("Before training: image encoder First Layer lora up", _up.weight.data)
-        print("Before training: image encoder First Layer lora down", _down.weight.data)
-        break
 
     if args.gradient_checkpointing:
         model.set_gradient_checkpointing(True)
@@ -90,11 +78,7 @@ def main(args):
 
     params_to_optimize = [
         {
-            "params": itertools.chain(*text_encoder_lora_params),
-            "lr": args.learning_rate,
-        },
-        {
-            "params": itertools.chain(*image_encoder_lora_params),
+            "params": itertools.chain(*model_lora_params),
             "lr": args.learning_rate,
         },
     ]
@@ -113,11 +97,9 @@ def main(args):
     total_steps = train_dataloader.num_batches * args.epochs
     scheduler = cosine_lr(optimizer, args.learning_rate, args.warmup, total_steps)
 
-    text_encoder, image_encoder, optimizer, scheduler = accelerator.prepare(
-        text_encoder, image_encoder, optimizer, scheduler
-    )
+    model, optimizer, scheduler = accelerator.prepare(model, optimizer, scheduler)
 
-    model.to(accelerator.device)
+    model.to(device=accelerator.device)
 
     print("***** Running training *****")
     print(f"  Num examples = {len(train_dataloader.dataset)}")
@@ -147,11 +129,14 @@ def main(args):
             total_loss = loss(image_features, text_features, logit_scale)
 
             accelerator.backward(total_loss)
+            if accelerator.sync_gradients:
+                params_to_clip = model.parameters()
+                accelerator.clip_grad_norm_(params_to_clip, 1.0)  # args.max_grad_norm)
 
             optimizer.step()
-            scheduler.step()
+            scheduler(step=global_step)
             optimizer.zero_grad()
-
+            progress_bar.update(1)
             global_step += 1
 
             if accelerator.is_local_main_process:
@@ -159,15 +144,31 @@ def main(args):
                     last_save = global_step
                     lora_save = f"{args.output_dir}/lora_weight_e{epoch}_s{global_step}.text_encoder.pt"
                     logger.info(f"Saving LoRA weights to {lora_save}")
-                    with torch.no_grad():
-                        save_lora_weight(
-                            unwrap_model(model).logit_scale.clamp_(0, math.log(100)),
-                            lora_save,
+
+                    save_lora_weight(
+                        accelerator.unwrap_model(model),
+                        lora_save,
+                    )
+                    for _up, _down in extract_lora_ups_down(
+                        model, target_replace_module=["MultiheadAttention"]
+                    ):
+                        print(
+                            f"Epoch {epoch} - Step {global_step}: text encoder First Layer lora up",
+                            _up.weight.data,
                         )
+                        print(
+                            f"Epoch {epoch} - Step {global_step}: text encoder First Layer lora down",
+                            _down.weight.data,
+                        )
+                        break
 
                     last_save = global_step
 
-            logs = {"loss": loss.detach().item(), "lr": scheduler.get_last_lr()[0]}
+            logs = {
+                "loss": total_loss.item(),
+                "lr": optimizer.param_groups[0]["lr"],
+                "epoch": epoch,
+            }
             progress_bar.set_postfix(**logs)
             accelerator.log(logs, step=global_step)
 
@@ -178,7 +179,9 @@ def main(args):
             f"{args.output_dir}/lora_weight_e{epoch}_s{global_step}.text_encoder.pt"
         )
         save_lora_weight(
-            unwrap_model(model).logit_scale.clamp_(0, math.log(100)), lora_save
+            # unwrap_model(model).logit_scale.clamp_(0, math.log(100)), lora_save
+            accelerator.unwrap_model(model),
+            lora_save,
         )
         logger.info(f"Saving LoRA weights to {lora_save}")
 
